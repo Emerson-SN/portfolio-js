a1823d697849b2d82c72ab0c914c0b07706d62b7
Emerson Santos <emerson_t92@hotmail.com>
2026-03-05 11:43:45 -0300

    nova confirmação

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 async function fetchProfileData() {
   const url =
-    'https://raw.githubusercontent.com/Emerson-SN/js-developer-portfolio/refs/heads/main/data/profile.json';
+    'https://raw.githubusercontent.com/Emerson-SN/portfolio-js/refs/heads/main/data/profile.json';
   const fetching = await fetch(url);
   const data = await fetching.json();
   console.log('JSON carregado:', data);
